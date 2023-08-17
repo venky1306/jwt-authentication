4cb40e1d505ff7511801bc83088a6dd1aee7f8df
venky1306 <47674519+venky1306@users.noreply.github.com>
2023-08-16 23:41:33 -0500

    added middleware for basic authorization

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,6 +1,35 @@
 package middleware
 
-// Authenticate is a middleware function to authenticate the user
-func Authenticate() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	helper "github.com/venky1306/jwt-authentication/helpers"
+)
+
+// Authenticate is a middleware function to authenticate the user
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("No Authorization headers provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+		c.Set("email", claims.Email)
+		c.Set("firstName", claims.First_name)
+		c.Set("lastName", claims.Last_name)
+		c.Set("uid", claims.Uid)
+		c.Set("user_type", claims.User_type)
+		c.Next()
+		fmt.Println(claims.Uid)
+	}
 }
